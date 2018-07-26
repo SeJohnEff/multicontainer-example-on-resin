73cbe162cdfb03832f8b1b970af2717a9b079aff
John Fornehed <johnfornehed@Johns-MacBook-Pro-2.local>
2018-07-27 01:27:20 +0200

    abc

--- simple-server-node/server.js
+++ simple-server-node/server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // reply to request with "Nu funkar det"
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Nu funkar det!!!!!!!');
 });
 
 //start a server on port 80 and log its start to our console
